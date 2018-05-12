fc1caebef4832084d770e0bc73a4b5eac88f8b8f
shashi-shivaraju <shashi2shivaraj@gmail.com>
2018-05-12 17:21:30 -0400

    code for PLY file rendering

---
+++ PLY_File_Triangle_Rendering/header.h
@@ -0,0 +1,89 @@
+/**********************************************************************
+*
+*  FILE NAME	: header.h
+*
+*  DESCRIPTION  : Header file
+* 
+*  PLATFORM		: Win32
+*
+*  DATE                 NAME                    REASON
+*  27th Feb,2018        ShashiShivaraju         ECE_6680_Lab_05
+*                       [C88650674]
+***********************************************************************/
+
+/*Header file inclusions*/ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>	/*included for strcmp() and memset() api*/
+#include <windows.h>
+#include <math.h>
+
+/*Macro definations*/
+#define MAX_8BIT	255
+#define MIN_8BIT	0		
+#define MAX_FILENAME	500
+
+#define PI_VALUE				3.14159265358979323846
+#define DEFAULT_ZBUFFER_DEPTH	999999
+#define d_THRESHOLD				0.0000000005
+
+#define CHECK_ANGLE(X) ((X>360 || X <-360)? -1:0)
+#define SQR(X)	X*X
+
+/*declarations of datatypes*/
+typedef struct __vertex_data
+{
+	unsigned int index;
+	float x_pos;
+	float y_pos;
+	float z_pos;
+}vertex_data,vector;
+
+/*declarations of datatypes*/
+typedef struct __face_data
+{
+	unsigned int index;
+	unsigned int total_vertices; /*Always 3 in triangle rendering*/
+	unsigned int vertex_one;
+	unsigned int vertex_two;
+	unsigned int vertex_three;
+}face_data;
+
+
+/*Function Prototypes*/
+
+/*Function to render triangles based on PLY file and camera roation angles*/
+void RenderTriangles(FILE * PLY_fpt,float X_Degree,float Y_Degree,float Z_Degree);
+
+/* Function to parse the PLY header to obtain number of vertices and number of faces*/
+void ParsePLYHeader(FILE * PLY_fpt,int *vertices,int *faces);
+
+/* Function to read the vertices and faces from the PLY file */
+void ReadPLY_Vertices_Faces(FILE * PLY_fpt,
+							unsigned int total_vertices,vertex_data** vertex_pointers,
+							unsigned int total_faces,face_data** face_pointers,
+							vertex_data *min_vertex,vertex_data *max_vertex);
+
+/* Function to initialize the rotation matrices*/
+void initialize_rotation_matrices(float rotation_x[3][3],float rotation_y[3][3],float rotation_z[3][3],
+								  float X_Degree,float Y_Degree,float Z_Degree);
+
+/*Function to multiply vector with a 3x3 rotation matrix*/
+void matrix_multiply(vector *cam_pos,float rotation[3][3],vector *out);
+
+/*Function to calculate cross product of two vectors*/
+void cross_product(vector *u,vector *v,vector *uxv);
+
+/*Function to calculate scalar product of two vectors*/
+float scalar_product(vector *u,vector *v);
+
+/*calculate the 3D coordinates bounding the image*/
+void calculate_3D_bounding_box_coordinates(vector *cam_pos,vertex_data *center_vertex,vector *up_pos,
+									 vector *left_pos,vector *right_pos,vector *top_pos,
+									 vector *bottom_pos,vector *top_left,vector *temp,
+									 float *a,float E);
+
+/*Function to convert pixel index to (x,y) coordinates of the image */
+int convert_index2height_width(int index,
+								int ROW,int COL,/* size of image */
+								int *x_pos,int *y_pos);/* pixel position */
---
+++ PLY_File_Triangle_Rendering/main.c
@@ -0,0 +1,101 @@
+/**********************************************************************
+*
+*  FILE NAME	: main.c
+*
+*  DESCRIPTION  : Implement triangle rendering for a PLY - Polygon File Format.
+* 
+*  PLATFORM		: Win32
+*
+*  DATE                 NAME                     REASON
+*  27th Feb,2018        Shashi Shivaraju         ECE_6680_Lab_05
+*                       [C88650674]
+***********************************************************************/
+
+/*Header file inclusions*/
+#include "header.h"
+
+int main(int argc, char *argv[])/*main function of the program*/
+{
+	FILE* fp = NULL;	/*file pointer to access the file*/
+	char filename[MAX_FILENAME] = {0};	/*character array to store the filename*/
+	unsigned int file_size = 0;	/*variable to store file size in bytes*/
+	float X_Degree = 0,Y_Degree = 0,Z_Degree = 0;
+	int ret =0;
+	char option = 0;
+
+ 	while(1)
+	{
+
+printf("*************************************\n\
+**************Triangle Rendering*****\n\
+*************************************\n");
+printf("Select usage\n\n\
+Enter 1 to process a PLY file\n\
+Enter 0 to Exit the program\n");
+
+		scanf(" %c",&option);
+
+		if('1' == option)
+		{
+			printf("\nEnter the filename\n");
+			scanf("%s",filename);
+
+			fp = fopen(filename,"r");
+			if(!fp)
+			{
+				printf("fopen failed for %s",filename);
+				return -1;
+			}
+
+			//fseek(fp, 0L, SEEK_END);
+			//file_size = ftell(fp);
+			//fseek(fp, 0L, SEEK_SET);
+			//printf("\nThe PLY file size is %d\n",file_size);
+
+			printf("\nEnter the rotation angles [X_Degree] [Y_Degree] [Z_Degree]\n");
+			scanf("%f %f %f",&X_Degree,&Y_Degree,&Z_Degree);
+
+			if(0 > CHECK_ANGLE(X_Degree) ||
+			   0 > CHECK_ANGLE(Y_Degree)||
+			   0 > CHECK_ANGLE(Z_Degree) )
+			{
+				printf("invalid angle entered!!!! Try Again");
+				option = '2'; /*setting it to invalid option to trigger retry*/
+			}	
+		}
+
+		switch(option)
+		{
+		case '1':
+			{
+				RenderTriangles(fp,X_Degree,Y_Degree,Z_Degree);
+			}
+			break;
+		case '0':
+			{
+				printf("\nexiting the program\n");
+			}
+			break;
+		default:
+			{
+				printf("\ninvalid option: %c;Retry\n",option);
+				break;
+			}
+		}
+
+		if(fp)
+		{
+			fclose(fp);
+			fp = NULL;
+		}
+
+		if('0' == option)
+		{
+			break;
+		}
+	}
+
+	Sleep(1000);
+
+	return 0;
+}
